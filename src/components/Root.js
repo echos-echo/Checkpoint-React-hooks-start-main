c486e9d6be8410d193fac8dd80200759c54363ca
Eve Cho <e.young.echo@gmail.com>
2022-08-21 16:07:00 -0400

    can load data fetched using axios. need to backtrack to fix a bug

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import PetList from './PetList';
+import axios from 'axios';
 
 // We'll render these sample pets for now. Later, we'll instead fetch the list
 // of pets from the server! We won't need samplePets after that.
-import samplePets from '../petdata';
+// import samplePets from '../petdata';
 
 const Root = () => {
+  const [petData, setPetData] = React.useState([]);
+
+  React.useEffect(() => {
+    const getPets = async () => {
+      const data = (await axios.get('/api/pets')).data;
+      setPetData(data);
+    };
+    getPets();
+  }, []);
 
   return (
     <>
       <h1>Adoption Center</h1>
-      <PetList pets={samplePets} />
+      <PetList pets={petData} />
     </>
   )
 }
