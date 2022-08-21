b595f52efbf264f897c80310fd9f9072c3453d54
Eve Cho <e.young.echo@gmail.com>
2022-08-21 13:05:52 -0400

    several pets render. dropdown options created, does not function yet

--- public/bundle.js
+++ public/bundle.js
@@ -15,6 +15,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _SinglePet__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./SinglePet */ "./src/components/SinglePet.js");
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 
 
 var cody = {
@@ -25,11 +37,68 @@ var cody = {
 }; // PetList only renders one SinglePet. We'd like it to render a list of pets,
 // passed in as props.pets. Don't forget to add a unique key to each one!
 
-function PetList() {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+function PetList(props) {
+  var _React$useState = react__WEBPACK_IMPORTED_MODULE_0___default().useState('all'),
+      _React$useState2 = _slicedToArray(_React$useState, 2),
+      selected = _React$useState2[0],
+      setSelected = _React$useState2[1];
+
+  react__WEBPACK_IMPORTED_MODULE_0___default().useEffect(function () {
+    switch (selected) {
+      case 'all':
+        console.log('all selected');
+        document.querySelector('.pet-list').appendChild = props.pets.map(function (pet) {
+          return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_SinglePet__WEBPACK_IMPORTED_MODULE_1__["default"], {
+            key: pet.id,
+            pet: pet
+          });
+        }).join('');
+        break;
+
+      case 'cats':
+        console.log('cats selected');
+        document.querySelector('.pet-list').appendChild = props.pets.map(function (pet) {
+          return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_SinglePet__WEBPACK_IMPORTED_MODULE_1__["default"], {
+            key: pet.id,
+            pet: pet
+          });
+        }).filter(function (pet) {
+          return pet.species === 'cat';
+        }).join('');
+        break;
+
+      case 'dogs':
+        console.log('dogs selected');
+        document.querySelector('.pet-list').appendChild = props.pets.map(function (pet) {
+          return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_SinglePet__WEBPACK_IMPORTED_MODULE_1__["default"], {
+            key: pet.id,
+            pet: pet
+          });
+        }).filter(function (pet) {
+          return pet.species === 'dog';
+        }).join('');
+        break;
+    }
+  }, [selected]);
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("select", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("option", {
+    onClick: function onClick() {
+      return setSelected('all');
+    }
+  }, "All"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("option", {
+    onClick: function onClick() {
+      return setSelected('cats');
+    }
+  }, "Cats"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("option", {
+    onClick: function onClick() {
+      return setSelected('dogs');
+    }
+  }, "Dogs")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
     className: "pet-list"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_SinglePet__WEBPACK_IMPORTED_MODULE_1__["default"], {
-    pet: cody
+  }, props.pets.map(function (pet) {
+    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_SinglePet__WEBPACK_IMPORTED_MODULE_1__["default"], {
+      key: pet.id,
+      pet: pet
+    });
   })));
 }
 
--- src/components/PetList.js
+++ src/components/PetList.js
@@ -10,12 +10,35 @@ const cody = {
 
 // PetList only renders one SinglePet. We'd like it to render a list of pets,
 // passed in as props.pets. Don't forget to add a unique key to each one!
-function PetList() {
+function PetList(props) {
+  const [selected, setSelected] = React.useState('all');
+
+  React.useEffect(() => {
+    switch (selected) {
+      case 'all':
+        console.log('all selected')
+        document.querySelector('.pet-list').appendChild = props.pets.map(pet => <SinglePet key={pet.id} pet={pet}/>).join('');
+        break;
+      case 'cats':
+        console.log('cats selected')
+        document.querySelector('.pet-list').appendChild = props.pets.map(pet => <SinglePet key={pet.id} pet={pet}/>).filter(pet => pet.species === 'cat').join('');
+        break;
+      case 'dogs':
+        console.log('dogs selected')
+        document.querySelector('.pet-list').appendChild = props.pets.map(pet => <SinglePet key={pet.id} pet={pet}/>).filter(pet => pet.species === 'dog').join('');
+        break;
+    }
+  }, [selected])
 
   return (
     <>
+      <select>
+        <option onClick={() => setSelected('all')}>All</option>
+        <option onClick={() => setSelected('cats')}>Cats</option>
+        <option onClick={() => setSelected('dogs')}>Dogs</option>
+      </select>
       <div className="pet-list">
-            <SinglePet  pet={cody}  />
+        {props.pets.map(pet => <SinglePet key={pet.id} pet={pet}/>)}
       </div>
     </>
   )
