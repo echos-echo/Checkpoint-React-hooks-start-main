092c2a5923b6d8d33eeb055932ea7666aa43da60
Eve Cho <e.young.echo@gmail.com>
2022-08-21 12:42:03 -0400

    renders in a single pet, its ininfo, and working toggle button for adoption status

--- src/components/SinglePet.js
+++ src/components/SinglePet.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-function SinglePet() {
+function SinglePet(props) {
+  const [adoptionStatus, setAdoptionStatus] = React.useState(false);
 
   return (
-    <div className={`single-pet`}>
-
+    // <div className={`single-pet`}>
+    <div className={adoptionStatus ? `single-pet adopted` : `single-pet` }>
+      <h1>{props.pet.name}</h1>
+      <p>{props.pet.species}</p>
+      <p>{props.pet.description}</p>
+      <hr/>
+      <p>{adoptionStatus ? "Adopted!" : "Available"}</p>
+      <button onClick={() => setAdoptionStatus(!adoptionStatus)}>Toggle Status</button>
     </div>
   );
 }
